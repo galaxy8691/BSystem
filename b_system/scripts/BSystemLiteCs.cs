363223317ac8d3c84fbafed14dc74775f58f0578
goip kam <yy20030716@msn.com>
2025-05-20 11:57:12 +0800

    CS now support mp

--- b_system/scripts/BSystemLiteCs.cs
+++ b_system/scripts/BSystemLiteCs.cs
@@ -5,14 +5,17 @@ using System.Collections.Generic;
 [GlobalClass]
 public partial class BSystemLiteCs : Node
 {
+	[Export]
+	public bool DisabledOnMpMode = false;
+
 	public enum ThreeStateBool{
 	TRUE = 0,
 	FALSE = 1,
 	NOTSET = 2
 	}
-	private Dictionary<string, Action> state_fns = new Dictionary<string, Action>();
-	private Dictionary<string, Action> init_when_change_state_fns = new Dictionary<string, Action>();
-	private Dictionary<string, object> blackboard = new Dictionary<string, object>();
+	private Dictionary<string, Action> m_StateFns = new Dictionary<string, Action>();
+	private Dictionary<string, Action> m_InitWhenChangeStateFns = new Dictionary<string, Action>();
+	private Dictionary<string, object> m_Blackboard = new Dictionary<string, object>();
 
 	[Export]
 	public Node Actor { get; set; }
@@ -22,14 +25,18 @@ public partial class BSystemLiteCs : Node
 
 	public override void _Ready()
 	{
+		if (DisabledOnMpMode && !IsMultiplayerAuthority()){
+			SetPhysicsProcess(false);
+			return;
+		}
 		InitCall();
 		ChangeState(InitState);
 	}
 
 	public void InsertState(string state, Action fn, Action init_fn)
 	{
-		state_fns[state] = fn;
-		init_when_change_state_fns[state] = init_fn;
+		m_StateFns[state] = fn;
+		m_InitWhenChangeStateFns[state] = init_fn;
 	}
 
 	protected virtual void InitCall()
@@ -38,26 +45,37 @@ public partial class BSystemLiteCs : Node
 
 	public void ChangeState(string state)
 	{
-		blackboard["current_state"] = state;
-		Action init_when_change_state_fn = init_when_change_state_fns[state];
-		init_when_change_state_fn();
+		if (DisabledOnMpMode && !IsMultiplayerAuthority()){
+			return;
+		}
+		m_Blackboard["current_state"] = state;
+		Action init_when_change_state_fn = m_InitWhenChangeStateFns[state];
+		if (init_when_change_state_fn != null){
+			init_when_change_state_fn();
+		}
 	}
 
 	public override void _PhysicsProcess(double delta)
 	{
-		var current_state = blackboard["current_state"] as string;
-		Action state_fn = state_fns[current_state];
+		var current_state = m_Blackboard["current_state"] as string;
+		Action state_fn = m_StateFns[current_state];
 		state_fn();
 	}
 
 	public virtual void SetBlackboard(string key, object value)
 	{
-		blackboard[key] = value;
+		if (DisabledOnMpMode && !IsMultiplayerAuthority()){
+			return;
+		}
+		m_Blackboard[key] = value;
 	}
 
 	public virtual object GetBlackboard(string key)
 	{
-		return blackboard[key];
+		if (DisabledOnMpMode && !IsMultiplayerAuthority()){
+			return null;
+		}
+		return m_Blackboard[key];
 	}
 
 }
