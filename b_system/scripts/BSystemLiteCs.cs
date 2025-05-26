47f4d30682dbf27673531c3374ca96e9e2892af2
goip kam <yy20030716@msn.com>
2025-05-26 17:07:57 +0800

    add fn state

--- b_system/scripts/BSystemLiteCs.cs
+++ b_system/scripts/BSystemLiteCs.cs
@@ -6,35 +6,7 @@ using System.Collections.Generic;
 public partial class BSystemLiteCs : Node
 {
 
-	public abstract class PackedState{
-		private string m_State;
-		private Node m_Actor;
-		private Action<string> m_ChangeStateFn;
-		protected Dictionary<string, object> m_Blackboard;
 
-		public PackedState(string state, Node actor, Action<string> change_state_fn, Dictionary<string, object> blackboard){
-			m_State = state;
-			m_Actor = actor;
-			m_ChangeStateFn = change_state_fn;
-			m_Blackboard = blackboard;
-		}
-
-		public void ChangeState(string state){
-			m_ChangeStateFn(state);
-		}
-
-		public Node GetActor(){
-			return m_Actor;
-		}
-
-		public string GetState(){
-			return m_State;
-		}
-
-		public abstract void StateFn();
-
-		public abstract void InitWhenChangeStateFn();
-	}
 	[Export]
 	public bool DisabledOnMpMode = false;
 
